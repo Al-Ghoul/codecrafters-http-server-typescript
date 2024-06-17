71665080576b559af8b536eb9182bd355754ac0a
Abdo .AlGhoul <Abdo.AlGhouul@gmail.com>
2024-06-17 13:17:46 +0300

    feat(endpoint): handle /files endpoint

--- app/main.ts
+++ app/main.ts
@@ -1,5 +1,14 @@
 import * as net from "net";
+import * as fs from "fs";
 
+let directory: string;
+
+process.argv.forEach(function(val, index, array) {
+  console.log(index + ': ' + val);
+  if (val === "--directory") {
+    directory = array[index + 1];
+  }
+});
 
 const server = net.createServer((socket) => {
   socket.on("close", () => {
@@ -22,6 +31,15 @@ const server = net.createServer((socket) => {
       case "user-agent":
         const userAgent = data.toString().substring(data.toString().indexOf("User-Agent")).split(' ')[1].trim();
         socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`);
+        break;
+      case "files":
+        try {
+          const content = fs.readFileSync(directory + urlSegments[2], "utf8");
+          socket.write(`HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${content.length}\r\n\r\n${content}`);
+        }
+        catch {
+          socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+        }
         break;
       default:
         socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
