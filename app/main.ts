e3a1bfbd6fe1c0ebeee21e746ef39fda1347a569
Abdo .AlGhoul <Abdo.AlGhouul@gmail.com>
2024-06-17 12:33:41 +0300

    feat(parse): parse urls
    handle / endpoint

--- app/main.ts
+++ app/main.ts
@@ -5,6 +5,22 @@ const server = net.createServer((socket) => {
   socket.on("close", () => {
     socket.end();
   });
+
+  socket.on("data", (data) => {
+    const requestData = data.toString().split(" ");
+    const url = requestData[1];
+
+    switch (url) {
+      case "/":
+        socket.write("HTTP/1.1 200 OK\r\n\r\n");
+        break;
+      default:
+        socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+    }
+
+    console.log(requestData);
+    socket.end();
+  });
 });
 
 server.listen(4221, "localhost");
