2895f809789591e83583d35dda2e5d35189a6983
Abdo .AlGhoul <Abdo.AlGhouul@gmail.com>
2024-06-17 12:39:37 +0300

    feat(echo-endpoint): parse url segments
    parse url segments & handle /echo endpoint

--- app/main.ts
+++ app/main.ts
@@ -8,11 +8,17 @@ const server = net.createServer((socket) => {
 
   socket.on("data", (data) => {
     const requestData = data.toString().split(" ");
-    const url = requestData[1];
+    const urlSegments = requestData[1].split("/");
+    const firstSegment = urlSegments[1];
+    console.log(urlSegments);
 
-    switch (url) {
-      case "/":
+    switch (firstSegment) {
+      case "":
         socket.write("HTTP/1.1 200 OK\r\n\r\n");
+        break;
+      case "echo":
+        const res = urlSegments[2];
+        socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${res.length}\r\n\r\n${res}`);
         break;
       default:
         socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
