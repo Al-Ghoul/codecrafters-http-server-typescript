20d3f7d1823833559feacf7dd9c0511aa379a714
Abdo .AlGhoul <Abdo.AlGhouul@gmail.com>
2024-06-17 12:46:27 +0300

    feat(endpoint): handle /user-agent endpoint

--- app/main.ts
+++ app/main.ts
@@ -10,7 +10,6 @@ const server = net.createServer((socket) => {
     const requestData = data.toString().split(" ");
     const urlSegments = requestData[1].split("/");
     const firstSegment = urlSegments[1];
-    console.log(urlSegments);
 
     switch (firstSegment) {
       case "":
@@ -19,6 +18,10 @@ const server = net.createServer((socket) => {
       case "echo":
         const res = urlSegments[2];
         socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${res.length}\r\n\r\n${res}`);
+        break;
+      case "user-agent":
+        const userAgent = data.toString().substring(data.toString().indexOf("User-Agent")).split(' ')[1].trim();
+        socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`);
         break;
       default:
         socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
